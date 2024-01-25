04689a431769bcedd66bd08e0c1a85cc3f4716f5
7pranit <pranitkonde007@gmail.com>
2024-01-25 11:37:38 +0530

    My personal commit to share

---
+++ src/com/rs/Veera.java
@@ -0,0 +1,19 @@
+package com.rs;
+
+public class Veera {
+	
+	public static void main(String[] args) {
+		
+		int year = 2024;
+		
+		if(year%4 == 0) {
+			
+			System.out.println("Year "+year+" is a Leap year");
+			
+		} else {
+			
+			System.out.println("Year "+year+" is not a Leap year");
+		}
+	}
+
+}
